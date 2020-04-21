b40203dd7a0f2767ccaeaaf938b6528c29e0ae79
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-04-21 14:40:15 -0400

    Updated folder structure

--- server/index.js
+++ server/index.js
@@ -1,34 +1,13 @@
 const express = require('express');
 const app = express();
-const passport = require('passport');
-const googleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 const port = process.env.PORT || 5000;
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
-passport.use(
-  new googleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
-    }
-  )
-);
+//Mount Routes
+app.use('/auth/google', authRoutes);
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+//Start Server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
