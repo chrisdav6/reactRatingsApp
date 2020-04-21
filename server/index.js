450c2cee09327cf3d76728d6fc7dc6f2ce65d6ff
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-04-21 15:52:30 -0400

    Created mongo user model

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
+
+//Connect to DB
+mongoose.connect(
+  keys.mongoURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to DB');
+  }
+);
 
 //Mount Routes
 app.use('/auth/google', authRoutes);
