4e4ace1baa79e82df1582b2ed214a817b70f969f
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-04-22 15:57:13 -0400

    Removed keys and updated

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,13 +1,4 @@
 module.exports = {
-  googleClientID:
-    '344438504190-pemmhf015lsidgkf9cnj5qdie470tg3i.apps.googleusercontent.com',
-  googleClientSecret: 'vMFK_OR9RcryfBVuZTvb5HH7',
-  mongoURL:
-    'mongodb+srv://chris:aUqiMG2eylcaRd5d@cluster0-3ovvq.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: 'fsoifjsdofpisdjfswpfgfaopsd'
-};
-
-module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_SECRET,
   mongoURL: process.env.MONGO_URL,
